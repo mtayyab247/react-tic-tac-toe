df8f9c06811676c8884c5bf7c6010bd4cd9c03a4
mtayyab247 <84352031+mtayyab247@users.noreply.github.com>
2021-11-07 20:36:46 -0600

    Added restart

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Footer from '../components/Footer';
-import Restart from '../components/Restart';
 import toJson from 'enzyme-to-json';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Footer from '../components/Footer';
+import Restart from '../components/Restart';
+import Result from '../components/Result';
 
 Enzyme.configure({
     adapter: new Adapter()
@@ -17,6 +18,11 @@ describe("<Footer /> test", () => {
 
     it("should render Restart", () => {
         const wrapper = shallow(<Restart />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("should render Result", () => {
+        const wrapper = shallow(<Result />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
