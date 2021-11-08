5a9479102051b2bc93af4446dd8f6f83a6f08b03
mtayyab247 <84352031+mtayyab247@users.noreply.github.com>
2021-11-07 20:47:16 -0600

    Added game result component

---
+++ src/components/GameResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameResult = () => {
+
+    return(
+        <div className="winner-wrapper">
+        </div>
+    );
+};
+
+export default GameResult;
--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -4,25 +4,25 @@ import toJson from 'enzyme-to-json';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Footer from '../components/Footer';
 import Restart from '../components/Restart';
-import Result from '../components/Result';
+import GameResult from '../components/GameResult';
 
 Enzyme.configure({
     adapter: new Adapter()
 });
 
 describe("<Footer /> test", () => {
-    it("should render Restart and Result", () => {
+    it("Should render Restart and Result", () => {
         const wrapper = shallow(<Footer />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it("should render Restart", () => {
+    it("Should render Restart", () => {
         const wrapper = shallow(<Restart />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 
-    it("should render Result", () => {
-        const wrapper = shallow(<Result />);
+    it("Should render Result", () => {
+        const wrapper = shallow(<GameResult />);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
 });
