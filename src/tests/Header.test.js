7c1badf3519498e2897f7ea6bf1537028617d6b7
mtayyab247 <84352031+mtayyab247@users.noreply.github.com>
2021-11-07 18:24:03 -0600

    Wrote passing test for header

---
+++ src/tests/Header.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Header from '../components/Header';
+import toJson from 'enzyme-to-json';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({
+    adapter: new Adapter()
+});
+
+describe("<Header /> test", () => {
+    it("should render title and player name whose turn it is", () => {
+        const wrapper = shallow(<Header playerTurn={"Player 1"} />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+});
+
+
