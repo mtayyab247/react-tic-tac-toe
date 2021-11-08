f37c64b9297e762c8a1e2162a0f09b13a28cd8d0
mtayyab247 <84352031+mtayyab247@users.noreply.github.com>
2021-11-07 20:53:50 -0600

    Added board component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Restart from './Restart';
+import GameResult from './GameResult';
 
 const Footer = () => {
 
     return(
         <div className="footer">
             <Restart />
+            <GameResult />
         </div>
     );
 };
