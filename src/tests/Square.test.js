1d555ddadb0d9125b03c245bf43f935c905caca9
mtayyab247 <84352031+mtayyab247@users.noreply.github.com>
2021-11-08 11:10:07 -0600

    Added test row and col attributes of Square

--- src/tests/Square.test.js
+++ src/tests/Square.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Square from '../components/Square';
@@ -35,6 +35,42 @@ describe("<Square /> test", () => {
         const wrapper = mount(<Square row={row} col={col} handleWinner={handleWinner} classAttr={classAttr} boardState={boardStates} />);
         wrapper.find('button').simulate('click');
         expect(handleWinner.mock.calls.length).toBe(1);
+    });
+
+    it("should show have row and col attribute", () => {
+        const row = 0;
+        const col = 0;
+        const classAttr = "bb";
+        const onSubmitSpy = jest.fn();
+        const boardStates = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+
+        const wrapper = mount(<Square row={row} col={col} handleWinner={onSubmitSpy} classAttr={classAttr} boardState={boardStates} />);
+        const rowVal = wrapper.find("button").props().row;
+        const colVal = wrapper.find("button").props().col;
+        expect(rowVal).toEqual(0);
+        expect(colVal).toEqual(0);
+    });
+
+    it("should show have row and col attribute", () => {
+        const row = 0;
+        const col = 0;
+        const classAttr = "bb";
+        const onSubmitSpy = jest.fn();
+        const boardStates = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+
+        const wrapper = mount(<Square row={row} col={col} handleWinner={onSubmitSpy} classAttr={classAttr} boardState={boardStates} />);
+        const rowVal = wrapper.find("button").props().row;
+        const colVal = wrapper.find("button").props().col;
+        expect(rowVal).toEqual(0);
+        expect(colVal).toEqual(0);
     });
 });
 
